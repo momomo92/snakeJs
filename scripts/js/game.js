13bdf1a97318eca214e21b74c10fcd68445a4614
NeMoMo <piotr@theworlt.com>
2018-03-07 22:56:54 +0100

    add points

--- scripts/js/game.js
+++ scripts/js/game.js
@@ -4,20 +4,22 @@ function Game(context, canvas) {
     this.context = context;
     this.snake = new Snake(this.context, this.canvas);
     this.food = new Food(this.context, this.canvas);
+    this.points = 0;
 
     this.init = function() {
-        this.startGame();
+        this.welcomeScreen();
     };
 
     this.startGame = function() {
         this.snake.init();
         this.food.init();
         this.intervalId = setInterval(function() {
-            console.info();
             context.clearRect(0, 0, canvas.width, canvas.height);
+            that.displayPoints();
 
             if (that.snake.ate) {
                 that.snake.eat();
+                that.points++;
                 that.food.isset = false;
                 that.snake.ate = false;
             }
@@ -37,10 +39,62 @@ function Game(context, canvas) {
         }, this.snake.speed);
     };
 
+    this.displayPoints = function() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.font = "10px Arial";
+        context.fillStyle = "red";
+        context.fillText("Point: " + this.points, 5, 10);
+    };
+
     this.endGame = function() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
         context.font = "16px Arial";
         context.fillStyle = "#0095DD";
-        context.fillText("GAME OVER", this.canvas.width / 2 - 60, this.canvas.height / 2);
+        context.fillText("GAME OVER", this.canvas.width / 2 - 60, 140);
+
+        context.font = "16px Arial";
+        context.fillStyle = "red";
+        context.fillText("Your points: " + this.points, this.canvas.width / 2 - 60, 170);
+
+        context.font = "16px Arial";
+        context.fillStyle = "red";
+        context.fillText("Pres enter to star game!", this.canvas.width / 2 - 100, 200);
         clearInterval(this.intervalId);
+        this.addKeyEvent(window);
     };
+
+    this.welcomeScreen = function() {
+        context.font = "18px Arial";
+        context.fillStyle = "#0095DD";
+        context.fillText("Welcome on my snake game!", this.canvas.width / 2 - 140, 140);
+
+        context.font = "16px Arial";
+        context.fillStyle = "red";
+        context.fillText("Pres enter to star game!", this.canvas.width / 2 - 110, 200);
+
+        this.addKeyEvent(window);
+    };
+
+    this.addKeyEvent =function(window) {
+        window.onkeydown = function(event) {
+            const keyCode = event.keyCode;
+
+            if (keyCode == 13) {
+                that.resetData();
+                that.startGame();
+            }
+
+            console.log(keyCode);
+        }
+    };
+
+    this.resetData = function() {
+        this.points = 0;
+        this.food.isset = false;
+        this.snake.alive = true;
+        this.snake.snakeLenght = 1;
+        this.snake.direction = this.snake.directions.right;
+        this.snake.ate = false;
+        this.snake.bodyPartsPosition = [];
+    }
 }
